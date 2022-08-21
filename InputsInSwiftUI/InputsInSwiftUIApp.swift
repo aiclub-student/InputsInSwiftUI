67c809b7a06c3e4a1b984bd3514d480437f18288
Amit Gupta <amit@gprof.com>
2022-08-21 14:28:14 -0700

    Initial Commit

---
+++ InputsInSwiftUI/InputsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputsInSwiftUIApp.swift
+//  InputsInSwiftUI
+//
+//  Created by Amit Gupta on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct InputsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
